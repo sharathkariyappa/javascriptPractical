19070e97fe2d4226670c7675f2ff474fe7e7419b
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-08 14:11:43 +0000

    conversion of datatypes

---
+++ .devcontainer/01_Basics/convsersion.js
@@ -0,0 +1,16 @@
+//Number conversion
+let score = "33abc";
+let changedType = Number(score);
+// console.log(changedType);
+
+//string conversion
+let stringData = "34";
+let changedData = String(stringData);
+// console.log(changedData);
+// console.log(typeof changedData);
+
+// bool conversion
+let ischanged =1;
+let changedBool = Boolean(ischanged);
+// console.log(typeof changedBool);
+// console.log(changedBool);
