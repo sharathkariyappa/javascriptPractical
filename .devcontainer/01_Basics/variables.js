85f0337f1c4e01633f18a633d1465ac08e934fdf
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-08 14:09:51 +0000

    variables usage

---
+++ .devcontainer/01_Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountPassword="12345"
+let accountEmail= "shaddy@gmail.com"
+
+
+// lets  make changes
+
+// accountId = 123
+accountEmail = "madman.com"
+accountPassword = "121"
+
+console.table([accountId,accountEmail,accountPassword]); //const can not be changed...... var do not used due to scope issues
