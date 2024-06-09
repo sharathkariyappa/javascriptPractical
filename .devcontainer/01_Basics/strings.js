97552bbdd0873a5f9f3d8aa85e03257086931ea0
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-09 13:45:23 +0000

    some practice on strings

---
+++ .devcontainer/01_Basics/strings.js
@@ -0,0 +1,15 @@
+let namee = "shanu ";
+let last = "for vacation";
+
+console.log(namee + last); //this is not the right way
+
+console.log(`hello come here ${namee} we will go outside ${last}`); //this is the optimal way
+
+const newName = new String("shara-ath");
+console.log(newName.length)
+console.log(newName.toUpperCase());
+
+const newstring = newName.substring(0,4);
+console.log(newstring);
+const newLine = newName.slice(0,2);
+console.log(newLine);
