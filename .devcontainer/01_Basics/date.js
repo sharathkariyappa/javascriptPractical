eca91785e63813eada095b444a827f49a39896bf
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-09 14:49:20 +0000

    practise on dates

---
+++ .devcontainer/01_Basics/date.js
@@ -0,0 +1,13 @@
+const day = new Date;
+console.log(day);
+console.log(day.toDateString()); // to get exact date
+
+// console.log(day.getDate());
+
+// console.log(day.toLocaleDateString());
+
+const day1 = new Date(2023,0,4);
+console.log(day1.toDateString());
+
+let tom = Date.now();
+console.log(day1.getDate());
