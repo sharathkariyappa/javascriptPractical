a0c644d6e4928406eb0594a6984758c21217c7a6
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-19 14:34:11 +0000

    controlflow using if and else condition

---
+++ .devcontainer/01_intermediate/controlFlow.js
@@ -0,0 +1,17 @@
+// controlflow / logic flow
+// way of stopping the code 
+
+// 1 if/else loops
+
+// const score = 100;
+// if(score >10) {
+//     console.log("score is grater than 10")
+// }
+// if(score>=100) {
+//     console.log("score is less than 100 and equal to 100")
+// }
+
+const username = "hello"
+if(username =="hello") {
+    console.log(`this is, ${username}`)
+}
