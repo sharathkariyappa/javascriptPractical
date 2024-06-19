80d6e0c549ae28acb3ec60f3ece676c2c82d3be7
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-19 14:37:54 +0000

    controlflow using switch condition

--- .devcontainer/01_intermediate/controlFlow.js
+++ .devcontainer/01_intermediate/controlFlow.js
@@ -11,7 +11,20 @@
 //     console.log("score is less than 100 and equal to 100")
 // }
 
-const username = "hello"
-if(username =="hello") {
-    console.log(`this is, ${username}`)
+// const username = "hello"
+// if(username =="hello") {
+//     console.log(`this is, ${username}`)
+// }
+
+// 2 switch loop
+
+const score = 100;
+switch (score) {
+    case 1:
+        console.log("score is not one")
+        break;
+    case 100:
+        console.log("score is 100")
+    default:
+        break;
 }
