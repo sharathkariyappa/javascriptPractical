de1c811ddc51356578fe7950e8419653bc7242b6
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-25 14:45:32 +0000

    while & do-while loop

---
+++ .devcontainer/04_Basics/whileanddo.js
@@ -0,0 +1,29 @@
+// while and do-while loops
+
+
+// let i= 0;
+// while (i<10) {
+//     // console.log(i);
+//     i=i+2;
+    
+// }
+
+// using array
+
+// let myArray = ["yash","prabhas","allu","vijay","ram","ntr"];
+
+// let i=0;
+// while (i<myArray.length) {
+//     console.log(`the stars are ${myArray[i]}`);
+//     i=i+1;
+// }
+
+// do -while loop
+
+let myArray = ["yash","prabhas","allu","vijay","ram","ntr"];
+let index = 0;
+do {
+    console.log(`the stars are ${myArray[index]}`);
+    index = index +1;
+    
+} while (index<myArray.length);
