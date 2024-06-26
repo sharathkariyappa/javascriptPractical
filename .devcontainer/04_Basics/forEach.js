5575c6b25799f966774496cad13d820cdfca0f6f
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-26 14:23:44 +0000

    applying foreach loop on array of objects

--- .devcontainer/04_Basics/forEach.js
+++ .devcontainer/04_Basics/forEach.js
@@ -2,18 +2,45 @@
 
 const myArray = ["kid","man","gay","uni","bis"]                                     // array
 
-myArray.forEach( function (items) {                                                 // for each loop take an call back function 
-    // console.log(items);                                                          //this is noraml function
-} )
+// myArray.forEach( function (items) {                                                 // for each loop take an call back function 
+//     // console.log(items);                                                          //this is noraml function
+// } )
 
 
-myArray.forEach( (items)=>{                                                        // this is arrow function
-    // console.log(items)
-})
+// myArray.forEach( (items)=>{                                                        // this is arrow function
+//     // console.log(items)
+// })
 
 // declare an seperate function
 
-function printme(items) {                                           // if we declare seperate function then also we can use that in forEach loop 
-    console.log(items);                                             // we can get an same output
-}
-myArray.forEach(printme)
+// function printme(items) {                                           // if we declare seperate function then also we can use that in forEach loop 
+//     // console.log(items);                                             // we can get an same output
+// }
+// myArray.forEach(printme)
+
+// myArray.forEach( (items,arr,index)=>{                                  // we can get an items,index and whole array in foreach loop
+//     // console.log(items,arr,index);
+// })
+
+
+// array of objects 
+//[{},{},{}]
+
+const array = [                                         //in array of objects
+    {
+        movie: "thor",
+        hero: "chris"
+    },
+    {
+        movie: "ironman",
+        hero: "RDJ"
+    },
+    {
+        movie: "kgf",
+        hero: "yash"
+    }
+]
+
+array.forEach( (items)=>{                               // we can get an object's values fro each  object in the array
+    console.log(items.hero);
+})
