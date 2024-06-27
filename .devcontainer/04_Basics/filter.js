6ab331fdf733f544074db95b99a233f0d9bae71f
sharathkariyappa <85184055+sharathkariyappa@users.noreply.github.com>
2024-06-27 13:36:28 +0000

    filter function use cases

--- .devcontainer/04_Basics/filter.js
+++ .devcontainer/04_Basics/filter.js
@@ -1,6 +1,6 @@
 // using filter
 
-const coding = ["java","javaScript","php","python","c++"]
+// const coding = ["java","javaScript","php","python","c++"]
 
 // coding.forEach( (languages) => {                                // if we do not want to store  the values in the variable
 //     console.log(languages);                                     // then we use forEach loop or iteraton
@@ -11,7 +11,19 @@ const coding = ["java","javaScript","php","python","c++"]
 // })
 // console.log(prog);                                                 // if we console this we get an undefined, to tackle this we use filter
 
-// filter.
+// filter.//
 
-const prog = coding.filter( (items) => {return items})    // here we declare variable to store result. // if we use parenthesis we must use return
-console.log(prog)                                                      // returns the values in array
+// const prog = coding.filter( (items) => {return items})    // here we declare variable to store result. // if we use parenthesis we must use return
+// console.log(prog)                                                      // returns the values in array
+
+
+// real -usage of filter function
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+let newNums = myNums.filter( (num) => {                       // use return inside the braces
+    return num > 4
+})
+// console.log(newNums)
+
+newNums = myNums.filter( (num) => num > 5 )                 // here not used braces so didnt used return keyword
+// console.log(newNums)
